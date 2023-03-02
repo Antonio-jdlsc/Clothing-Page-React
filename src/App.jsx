f06710237f35eab488c540a0792eb4456adaadd3
Antonio <antonio.jdlsc@gmail.com>
2023-03-02 14:40:33 -0600

    Code added to the last components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+import NarvaroInfo from './Components/NarvaroInfo';
+import CollectionForm from './Components/CollectionForm';
+import Footer from './Components/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const firebaseConfig = {
+  // Credenciales
 }
 
-export default App
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  return (
+    <div>
+      <NarvaroInfo />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Dirección de la marca</p>
+      <p>Teléfono de contacto de la marca</p>
+    </footer>
+  );
+}
+
+export default Footer;
