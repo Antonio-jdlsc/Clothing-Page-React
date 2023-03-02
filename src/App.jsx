dce304e981a39b5f87607ce1e7e1b684aea15bba
Antonio <antonio.jdlsc@gmail.com>
2023-03-02 17:02:40 -0600

    Creating the credentials for firebase connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { initializeApp, firebase } from "firebase/compat/app";
+import 'firebase/compat/firestore';
 import NarvaroInfo from './Components/NarvaroInfo';
 import CollectionForm from './Components/CollectionForm';
 import Footer from './Components/Footer';
 import ImageCard from './Components/ImageCards'
 
 const firebaseConfig = {
-  // Credenciales
+  apiKey: "AIzaSyCvgvZpx_xqLOJl220s5TY5IzH2fllhGGQ",
+  authDomain: "crud-app-firebase-cc8bd.firebaseapp.com",
+  projectId: "crud-app-firebase-cc8bd",
+  appId: "1:965541733498:web:55a1c4a9d1810a161f47c3",
+  collection: "vOXoZguRLnZSqi1demK6"
 }
 
 firebase.initializeApp(firebaseConfig);
@@ -16,6 +20,7 @@ function App() {
     'https://via.placeholder.com/1200x600/000000/FFFFFF/?text=Imagen+1',
     'https://via.placeholder.com/1200x600/000000/FFFFFF/?text=Imagen+2',
   ]
+
   return (
     <div>
       <NarvaroInfo />
